58e54e34b5c292a65b3ad6fcf5d438eb940e939b
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-11-17 22:15:31 +0100

    Dodani dummy podaci u prisustvo da bi tabela izgledala što približnije tabeli iz prethodne spirale

--- scripts/prisustvo.js
+++ scripts/prisustvo.js
@@ -2,37 +2,130 @@ let div = document.getElementById("divSadrzaj");
 //instanciranje
 let prisustvo = TabelaPrisustvo(div, {
 	"studenti": [{
-			"ime": "Neko Nekic",
+			"ime": "Neko Nekić",
 			"index": 12345
 		},
 		{
-			"ime": "Drugi Neko",
-			"index": 12346
-		}
+			"ime": "Neko Nekić 1",
+			"index": 23441
+		},
+        {
+            "ime": "Neko nekić 3",
+            "index": 53545
+        }
 	],
 	"prisustva": [{
-			"sedmica": 3,
-			"predavanja": 2,
+			"sedmica": 1,
+			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 12346
+			"index": 12345
 		},
-		
 		{
-			"sedmica": 2,
-			"predavanja": 2,
+			"sedmica": 1,
+			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 12346
+			"index": 23441
 		},
         {
 			"sedmica": 1,
 			"predavanja": 2,
 			"vjezbe": 2,
+			"index": 53545
+		},
+        {
+			"sedmica": 2,
+			"predavanja": 3,
+			"vjezbe": 2,
 			"index": 12345
-		}
+		},
+		{
+			"sedmica": 2,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 23441
+		},
+        {
+			"sedmica": 2,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 53545
+		},
+        {
+			"sedmica": 3,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 12345
+		},
+		{
+			"sedmica": 3,
+			"predavanja": 1,
+			"vjezbe": 0,
+			"index": 23441
+		},
+        {
+			"sedmica": 3,
+			"predavanja": 0,
+			"vjezbe": 1,
+			"index": 53545
+		},
+        {
+			"sedmica": 4,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 12345
+		},
+		{
+			"sedmica": 4,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 23441
+		},
+        {
+			"sedmica": 4,
+			"predavanja": 1,
+			"vjezbe": 2,
+			"index": 53545
+		},
+        {
+			"sedmica": 5,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 12345
+		},
+		{
+			"sedmica": 5,
+			"predavanja": 1,
+			"vjezbe": 0,
+			"index": 23441
+		},
+        {
+			"sedmica": 5,
+			"predavanja": 0,
+			"vjezbe": 1,
+			"index": 53545
+		},
+        {
+			"sedmica": 6,
+			"predavanja": 3,
+			"vjezbe": 0,
+			"index": 12345
+		},
+		{
+			"sedmica": 6,
+			"predavanja": 1,
+			"vjezbe": 2,
+			"index": 23441
+		},
+        {
+			"sedmica": 6,
+			"predavanja": 1,
+			"vjezbe": 2,
+			"index": 53545
+		}				
 	],
-	"predmet": "Razvoj mobilnih aplikacija",
-	"brojPredavanjaSedmicno": 5,
-	"brojVjezbiSedmicno": 6
+	"predmet": "P1",
+	"brojPredavanjaSedmicno": 3,
+	"brojVjezbiSedmicno": 2
 }
 );
 
