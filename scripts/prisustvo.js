9d365e9e290f39092466bfbea4e1665ae87846d6
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-11-25 14:39:39 +0100

    Nakon testiranja vraćeni dummy podaci za sličnost spirali 1!

--- scripts/prisustvo.js
+++ scripts/prisustvo.js
@@ -1,77 +1,131 @@
 let div = document.getElementById("divSadrzaj");
+
 //instanciranje
 let prisustvo = TabelaPrisustvo(div, {
 	"studenti": [{
-			"ime": "Student A",
-			"index": 1
+			"ime": "Neko Nekić",
+			"index": 12345
 		},
 		{
-			"ime": "Student B",
-			"index": 2
+			"ime": "Neko Nekić 1",
+			"index": 23441
 		},
         {
             "ime": "Neko nekić 3",
             "index": 53545
-        },
-        {
-            "ime": "Neko nekić 3",
-            "index": 60
-        },
-        {
-            "ime": "Vanredni student",
-            "index": 999
         }
 	],
-	"prisustva": [
-        {
-            "sedmica": 5,
-            "predavanja": 4,
-            "vjezbe": 4,
-            "index": 999
-        },
-        {
-			"sedmica": 4,
+	"prisustva": [{
+			"sedmica": 1,
 			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 1
+			"index": 12345
 		},
-        {
-			"sedmica": 2,
+		{
+			"sedmica": 1,
 			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 1
+			"index": 23441
 		},
         {
 			"sedmica": 1,
-			"predavanja": 0,
-			"vjezbe": 0,
-			"index": 1
+			"predavanja": 2,
+			"vjezbe": 2,
+			"index": 53545
 		},
         {
 			"sedmica": 2,
 			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 2
+			"index": 12345
+		},
+		{
+			"sedmica": 2,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 23441
+		},
+        {
+			"sedmica": 2,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 53545
 		},
         {
 			"sedmica": 3,
 			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 2
+			"index": 12345
+		},
+		{
+			"sedmica": 3,
+			"predavanja": 1,
+			"vjezbe": 0,
+			"index": 23441
 		},
         {
-			"sedmica": 1,
+			"sedmica": 3,
+			"predavanja": 0,
+			"vjezbe": 1,
+			"index": 53545
+		},
+        {
+			"sedmica": 4,
 			"predavanja": 3,
 			"vjezbe": 2,
-			"index": 2
-		}
+			"index": 12345
+		},
+		{
+			"sedmica": 4,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 23441
+		},
+        {
+			"sedmica": 4,
+			"predavanja": 1,
+			"vjezbe": 2,
+			"index": 53545
+		},
+        {
+			"sedmica": 5,
+			"predavanja": 3,
+			"vjezbe": 2,
+			"index": 12345
+		},
+		{
+			"sedmica": 5,
+			"predavanja": 1,
+			"vjezbe": 0,
+			"index": 23441
+		},
+        {
+			"sedmica": 5,
+			"predavanja": 0,
+			"vjezbe": 1,
+			"index": 53545
+		},
+        {
+			"sedmica": 6,
+			"predavanja": 3,
+			"vjezbe": 0,
+			"index": 12345
+		},
+		{
+			"sedmica": 6,
+			"predavanja": 1,
+			"vjezbe": 2,
+			"index": 23441
+		},
+        {
+			"sedmica": 6,
+			"predavanja": 1,
+			"vjezbe": 2,
+			"index": 53545
+		}				
 	],
 	"predmet": "P1",
-	"brojPredavanjaSedmicno": 6,
-	"brojVjezbiSedmicno": 6
+	"brojPredavanjaSedmicno": 3,
+	"brojVjezbiSedmicno": 2
 }
 );
-
-//pozivanje metoda - ove metode kasnije implementirati
-//prisustvo.sljedecaSedmica();
-//prisustvo.prethodnaSedmica();
