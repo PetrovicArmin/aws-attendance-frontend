8ce017fabdcf1ffea16737d75fd43d98c50346f7
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-12-28 09:11:43 +0100

    Refaktoring projekta za serviranje dosadašnjih funkcionalnosti pomoću express servera

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = __filename.substring(0, __filename.lastIndexOf('/'));
+const public_folder = path.join(__dirname, "public");
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/predmet', (req, res) => {
+    res.sendFile("html/predmet.html", { root: public_folder });
+});
+
+app.get('/prisustvo', (req, res) => {
+    res.sendFile("html/prisustvo.html", { root: public_folder });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
