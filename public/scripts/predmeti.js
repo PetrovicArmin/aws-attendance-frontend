9453492b159d99c8ce24a0a1922d30a7e5d85332
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-12-30 01:07:28 +0100

    Preusmjerenje na prisustvo.html da bismo tamo prikazali tabelu!

--- public/scripts/predmeti.js
+++ public/scripts/predmeti.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+const initialActions = () => {
     PoziviAjax.getPredmeti((err, data) => {
         if (err) {
             console.log(err);
@@ -16,12 +16,12 @@ window.onload = () => {
         const predmetiUl = document.getElementById('predmeti');
         predmetiUl.innerHTML = "";
 
-        //TODO: dodati onclick mogućnost radi promjene tabele koja dolazi u naš prozor!
-        //nakon toga, na click dodati još jednu funkciju koja će iscrtavati tabelu unutar našeg prozora!
         for (let predmet of data.predmeti) 
             predmetiUl.innerHTML += `<li class="nav-item" onclick="PoziviAjax.getPredmet('${predmet}', callbackPredmet)"> \n <a class="nav-link" href="#">${predmet}</a> \n </li>`;
     });
 };
+
+initialActions();
 
 const callbackLogout = (err, data) => {
     if(err) { 
@@ -41,18 +41,8 @@ const callbackPredmet = (err, data) => {
         console.log(data.poruka)
         return;
     }
-    TabelaPrisustvoClickable(document.getElementById('divSadrzaj'), data.prisustvo);
+
+    localStorage.setItem('prisustvo', JSON.stringify(data.prisustvo));
+    window.location.replace("http://localhost:3000/prisustvo.html");
 }
 
-const prisustvaCallback = (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }   
-
-    if (data.poruka) {
-        console.log(data.poruka);
-        return;
-    }
-    TabelaPrisustvoClickable(document.getElementById('divSadrzaj'), data.prisustvo, data.sedmica);
-}
--- public/scripts/prisustvo.js
+++ public/scripts/prisustvo.js
@@ -1,131 +1,16 @@
 let div = document.getElementById("divSadrzaj");
+const prisustvo = JSON.parse(localStorage.getItem('prisustvo'));
+TabelaPrisustvoClickable(div, prisustvo);
 
-//instanciranje
-let prisustvo = TabelaPrisustvo(div, {
-	"studenti": [{
-			"ime": "Neko Nekić",
-			"index": 12345
-		},
-		{
-			"ime": "Neko Nekić 1",
-			"index": 23441
-		},
-        {
-            "ime": "Neko nekić 3",
-            "index": 53545
-        }
-	],
-	"prisustva": [{
-			"sedmica": 1,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 12345
-		},
-		{
-			"sedmica": 1,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 23441
-		},
-        {
-			"sedmica": 1,
-			"predavanja": 2,
-			"vjezbe": 2,
-			"index": 53545
-		},
-        {
-			"sedmica": 2,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 12345
-		},
-		{
-			"sedmica": 2,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 23441
-		},
-        {
-			"sedmica": 2,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 53545
-		},
-        {
-			"sedmica": 3,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 12345
-		},
-		{
-			"sedmica": 3,
-			"predavanja": 1,
-			"vjezbe": 0,
-			"index": 23441
-		},
-        {
-			"sedmica": 3,
-			"predavanja": 0,
-			"vjezbe": 1,
-			"index": 53545
-		},
-        {
-			"sedmica": 4,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 12345
-		},
-		{
-			"sedmica": 4,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 23441
-		},
-        {
-			"sedmica": 4,
-			"predavanja": 1,
-			"vjezbe": 2,
-			"index": 53545
-		},
-        {
-			"sedmica": 5,
-			"predavanja": 3,
-			"vjezbe": 2,
-			"index": 12345
-		},
-		{
-			"sedmica": 5,
-			"predavanja": 1,
-			"vjezbe": 0,
-			"index": 23441
-		},
-        {
-			"sedmica": 5,
-			"predavanja": 0,
-			"vjezbe": 1,
-			"index": 53545
-		},
-        {
-			"sedmica": 6,
-			"predavanja": 3,
-			"vjezbe": 0,
-			"index": 12345
-		},
-		{
-			"sedmica": 6,
-			"predavanja": 1,
-			"vjezbe": 2,
-			"index": 23441
-		},
-        {
-			"sedmica": 6,
-			"predavanja": 1,
-			"vjezbe": 2,
-			"index": 53545
-		}				
-	],
-	"predmet": "P1",
-	"brojPredavanjaSedmicno": 3,
-	"brojVjezbiSedmicno": 2
+const prisustvaCallback = (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }   
+
+    if (data.poruka) {
+        console.log(data.poruka);
+        return;
+    }
+    TabelaPrisustvoClickable(div, data.prisustvo, data.sedmica);
 }
-);
